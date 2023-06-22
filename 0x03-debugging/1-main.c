07cb2bf5bbe90d826f3ecdf3c97e3a64ebbc7e79
ayoub belmayzi <ayoubelmayzi@gmail.com>
2023-06-22 07:07:55 -0700

    hello

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/* while (i < 10)*/
-	/*{*/
-	/*  putchar(i);*/
-	/*}*/
+	/* {*/
+	/* putchat(i);*/
+	/* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
